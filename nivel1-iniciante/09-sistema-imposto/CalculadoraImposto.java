cb2f82fa1199d7e42798191302d019b29ee08040
pedro <pedro@DBRUDOMAIN.CORP>
2026-09-10 16:44:51 -0300

    feat: conclusão do ex 09

--- nivel1-iniciante/09-sistema-imposto/CalculadoraImposto.java
+++ nivel1-iniciante/09-sistema-imposto/CalculadoraImposto.java
@@ -6,18 +6,46 @@ public class CalculadoraImposto {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Seja bem vindo ao Sistema de Calculadora de Imposto!");
-        Double salarioBruto = scanner.nextDouble();
         System.out.println("Digite seu Salario Bruto: ");
-        if (salarioBruto <= 2000){
-            System.out.println("Isento (0%)");
-        } else if (salarioBruto <= 3000) {
-            System.out.println("Valor do imposto sera de 7,5%");
-        } else if (salarioBruto <= 4500) {
-            System.out.println("Valor do imposto sera de 15%");
-        } else if (salarioBruto <= 6000) {
-            System.out.println("Valor do imposto sera de 22,5%");
-        }else {
-            System.out.println("Valor do imposto sera de 27,5%");
+        Double salarioBruto = scanner.nextDouble();
+        double calcValorliquido = 0;
+        double calcNumber = 0;
+
+        if (salarioBruto <= 0){
+            System.out.println("Valor invalido. Digite valor positivo ou maior que 0");
+        }
+        else {
+            if (salarioBruto <= 2000){
+                System.out.println("Isento (0%)");
+            } else if (salarioBruto <= 3000) {
+
+                System.out.println("Valor do imposto sera de 7,5%");
+                calcNumber = (salarioBruto * 7.5) / 100;
+                calcValorliquido = salarioBruto - calcNumber;
+
+            } else if (salarioBruto <= 4500) {
+
+                System.out.println("Valor do imposto sera de 15%");
+                calcNumber = (salarioBruto * 15) / 100;
+                calcValorliquido = salarioBruto - calcNumber;
+
+
+            } else if (salarioBruto <= 6000) {
+
+                System.out.println("Valor do imposto sera de 22,5%");
+                calcNumber = (salarioBruto * 22.5) / 100;
+                calcValorliquido = salarioBruto - calcNumber;
+
+            }else {
+
+                System.out.println("Valor do imposto sera de 27,5%");
+                calcNumber = (salarioBruto * 27.5) / 100;
+                calcValorliquido = salarioBruto - calcNumber;
+
+            }
+            System.out.printf("Desconto sera no valor de: RS$%.2f%n", calcNumber);
+            System.out.printf("Salario Liquido: RS$%.2f%n", calcValorliquido);
+
         }
     }
 }
